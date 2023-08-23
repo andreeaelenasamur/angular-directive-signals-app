461f62063b1c8562de20274d53895465a4cbf04f
Andreea <andelenasamur@gmail.com>
2023-08-23 10:58:43 +0200

    show errors

--- src/app/shared/directives/custom-label/custom-label.directive.ts
+++ src/app/shared/directives/custom-label/custom-label.directive.ts
@@ -1,4 +1,6 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { MinLengthValidator, RequiredValidator, ValidationErrors } from '@angular/forms';
+import { min } from 'rxjs';
 
 @Directive({
   selector: '[customLabel]'
@@ -7,11 +9,17 @@ export class CustomLabelDirective implements OnInit {
 
   private htmlElement?: ElementRef<HTMLElement>;
   private _color:string = 'red';
+  private _errors?: ValidationErrors | null;
 
   @Input() set color(value:string) {
     this._color = value;
     this.setStyle();
   }
+
+  @Input() set errors(value: ValidationErrors | null | undefined) {
+    this._errors = value;
+    this.setErrorMessage();
+    }
 
   constructor( private el: ElementRef<HTMLElement> ) {
     // console.log('constructor de la directiva')
@@ -26,5 +34,35 @@ export class CustomLabelDirective implements OnInit {
   setStyle():void {
     if ( !this.htmlElement ) return;
     this.htmlElement!.nativeElement.style.color = this._color;
+  }
+
+  setErrorMessage():void {
+    if( !this.htmlElement ) return;
+    if( !this._errors ){
+      this.htmlElement.nativeElement.innerText = '';
+      return;
+    }
+
+    const errors = Object.keys(this._errors);
+    console.log(errors)
+
+    if ( errors.includes('required') ){
+      this.htmlElement.nativeElement.innerText = 'Este campo es requerido.';
+      return;
+    }
+
+    if ( errors.includes('minlength')){
+      const min = this._errors!['minlength']['requiredLength'];
+      const current = this._errors!['minlength']['actualLength'];
+
+      this.htmlElement.nativeElement.innerText = `Mínimo ${current}/${min} caracteres.`;
+      return;
+    }
+
+
+    if ( errors.includes('email') ){
+      this.htmlElement.nativeElement.innerText = `Este email no es válido.`;
+      return;
+    }
   }
 }
