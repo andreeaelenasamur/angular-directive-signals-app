f1d24f913165a5135684ceca6504c8463e6b8efc
Andreea <andelenasamur@gmail.com>
2023-08-23 16:29:50 +0200

    effects with signals

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,11 +1,13 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, computed, effect, signal } from '@angular/core';
 import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './properties-page.component.html',
   styleUrls: ['./properties-page.component.css']
 })
-export class PropertiesPageComponent {
+export class PropertiesPageComponent implements OnDestroy, OnInit{
+
+  public counter = signal(10);
 
   public user = signal<User>({
     id: 2,
@@ -16,6 +18,29 @@ export class PropertiesPageComponent {
   });
 
   public fullName = computed( () => `${this.user().first_name} ${this.user().last_name}`)
+
+  public userChangedEffect = effect( () => {
+
+    console.log(`${this.user().first_name} - ${this.counter()}`);
+
+  });
+
+  ngOnInit(): void {
+    setInterval( () => {
+      this.counter.update( current => current + 1 );
+
+      if ( this.counter() == 15 )
+      this.userChangedEffect.destroy();
+    }, 1000)
+  }
+
+  ngOnDestroy(): void {
+    // this.userChangedEffect.destroy();
+  }
+
+  increaseBy(value: number) {
+    this.counter.update( current => current + value)
+  }
 
 
   onFieldUpdated( field: keyof User, value: string ) {
